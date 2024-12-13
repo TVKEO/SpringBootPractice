cf7f511f8e206540bed70150afde806f42528c93
TVKEO <t.nagatani.cingroup2012@gmail.com>
2024-12-13 19:25:42 +0900

    Revert "form_final"
    This reverts commit b2fdfa39b016a08438514f9d84e541792966b4a9.

--- practice/src/main/java/com/example/demo/PracticeApplication.java
+++ practice/src/main/java/com/example/demo/PracticeApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class LoginApplication {
+public class PracticeApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(LoginApplication.class, args);
+		SpringApplication.run(PracticeApplication.class, args);
 	}
 
 }
