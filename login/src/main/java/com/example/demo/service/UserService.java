afb7c018b11486417e8c92124e09f13c122ee65a
TVKEO <t.nagatani.cingroup2012@gmail.com>
2024-12-20 20:49:04 +0900

    プロジェクト統一と修正

--- login/src/main/java/com/example/demo/service/UserService.java
+++
@@ -1,8 +0,0 @@
-package com.example.demo.service;
-
-import com.example.demo.form.SignupForm;
-
-public interface UserService {
-    String authenticateUser(String email, String password);
-    String addUser(SignupForm form);
-}
